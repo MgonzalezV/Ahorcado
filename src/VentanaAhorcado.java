c9c3775034b568f35342f519f26db64333a15eb4
MARCOS <MARCOS@MARCOS-PC>
2015-01-19 11:14:30 +0100

    Version final Lunes 19/01

--- src/VentanaAhorcado.java
+++ src/VentanaAhorcado.java
@@ -1,4 +1,10 @@
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 
 /*
@@ -17,6 +23,36 @@ public class VentanaAhorcado extends javax.swing.JFrame {
     
     //contador para el numero de fallos
     int numeroFallos = 0;
+    
+    @Override
+    public void paint (Graphics g){
+        super.paintComponents(g);
+        g = jPanel1.getGraphics();
+        
+        //Cargamos una imagen
+        
+        Image miImagen = null;
+        
+        try {
+            
+            switch (numeroFallos){
+                    case 0: miImagen = ImageIO.read(getClass().getResource("/ahorcado_0.png")); break;
+                    case 1: miImagen = ImageIO.read(getClass().getResource("/ahorcado_1.png")); break;
+                    case 2: miImagen = ImageIO.read(getClass().getResource("/ahorcado_2.png")); break;
+                    case 3: miImagen = ImageIO.read(getClass().getResource("/ahorcado_3.png")); break;
+                    case 4: miImagen = ImageIO.read(getClass().getResource("/ahorcado_4.png")); break;
+                    case 5: miImagen = ImageIO.read(getClass().getResource("/ahorcado_5.png")); break;
+                    case -100: miImagen = ImageIO.read(getClass().getResource("/acertasteTodo.png")); break;
+                    default: miImagen = ImageIO.read(getClass().getResource("/ahorcado_fin.png")); break;
+                  
+            
+            }
+        
+        } catch (IOException ex) {
+           
+        }
+        g.drawImage(miImagen, 0, 0, jPanel1.getWidth(), jPanel1.getHeight(), null);
+    }
     
     //este es el constructor
     public VentanaAhorcado() {
@@ -48,6 +84,14 @@ public class VentanaAhorcado extends javax.swing.JFrame {
          numeroFallos++;
          jLabel2.setText(String.valueOf(numeroFallos));
       }
+      
+      //para ver si todas las letras están descubiertas
+      if(!palabraConGuiones.contains("_")){
+          numeroFallos = -100;
+      }
+      
+      
+      repaint();
     }
     
     
